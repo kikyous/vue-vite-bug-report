c9b5f6f7d8d41dfb87e5269f21526ea3f5b2e063
chen <kikyous@163.com>
2022-03-30 23:45:39 +0800

    init

---
+++ src/App-no-error.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="p-5">
+      <input
+        v-model="name"
+        type="text"
+      >
+      <div v-if="!name.length" class="text-sm text-red-400">
+        error
+      </div>
+      <div v-if="showTable" class="">
+        <ElTable :data="[]" style="width: 100%">
+          <ElTableColumn width="100">
+            <template #default="{row}">
+              <span>{{ row.createdBy.name }}</span>
+            </template>
+          </ElTableColumn>
+        </ElTable>
+      </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import { ElTable, ElTableColumn } from 'element-plus'
+
+const showTable = ref(false)
+
+const name = ref('')
+setTimeout(() => {
+  showTable.value = true
+}, 500)
+
+</script>
